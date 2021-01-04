a08a6dd8738353cf0fb307900981aa9f1fd78f32
abdul farhan <kukluu@hotmail.com>
2021-01-04 12:02:40 -0800

    get request successful & update request successful on postman

--- src/api/logs.js
+++ src/api/logs.js
@@ -3,10 +3,13 @@ const { Router } = require("express");
 const router = Router();
 const LogEntry = require("../model/LogEntry");
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "🌎",
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
@@ -20,5 +23,46 @@ router.post("/", async (req, res, next) => {
     }
     next(error);
   }
+});
+
+// router.post("/:id", async (req, res, next) => {
+//   try {
+//     LogEntry.findById(req.params.id);
+//     const logUpdate = await LogEntry.then((logentry) => {
+//       logentry.title = req.body.title;
+//       logentry.description = req.body.description;
+//       logentry.comments = req.body.comments;
+//       logentry.image = req.body.image;
+//       logentry.rating = req.body.rating;
+//       logentry.latitude = req.body.latitude;
+//       logentry.longitude = req.body.longitude;
+//       logentry.visitDate = req.body.visitDate;
+//     });
+
+//     const updatedLog = await logUpdate.save();
+//     res.json(updatedLog);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+router.post("/:id", (req, res, next) => {
+  LogEntry.findById(req.params.id)
+    .then((logentry) => {
+      logentry.title = req.body.title;
+      logentry.description = req.body.description;
+      logentry.comments = req.body.comments;
+      logentry.image = req.body.image;
+      logentry.rating = req.body.rating;
+      logentry.latitude = req.body.latitude;
+      logentry.longitude = req.body.longitude;
+      logentry.visitDate = req.body.visitDate;
+
+      logentry
+        .save()
+        .then(res.json("updated"))
+        .catch((err) => res.status(400).json(err));
+    })
+    .catch((err) => res.status(400).json(err));
 });
 module.exports = router;
